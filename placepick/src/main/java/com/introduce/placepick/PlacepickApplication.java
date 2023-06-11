cce90363b5847b89651dcb88dc213c8ef0cb7980
youngyoung0 <taeyoung6873@gmail.com>
2023-06-11 22:28:25 +0900

    project init

---
+++ placepick/src/main/java/com/introduce/placepick/PlacepickApplication.java
@@ -0,0 +1,13 @@
+package com.introduce.placepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacepickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacepickApplication.class, args);
+	}
+
+}
---
+++ placepick/src/test/java/com/introduce/placepick/PlacepickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introduce.placepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacepickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
